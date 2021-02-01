c128861afd41d616fbd67f9df08cd72306643acd
kunihei <tomarino080@gmail.com>
2021-02-02 08:45:03 +0900

    Initial Commit

---
+++ Swift5Senni1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5Senni1
+//
+//  Created by 祥平 on 2021/02/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
