f172b094e842af3547bab7d5b653cd0248fc4f71
kunihei <tomarino080@gmail.com>
2021-02-02 20:38:16 +0900

    dismissを使った画面遷移の仕方についてのコード

---
+++ Swift5Senni1/NextViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NextViewController.swift
+//  Swift5Senni1
+//
+//  Created by 祥平 on 2021/02/02.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
